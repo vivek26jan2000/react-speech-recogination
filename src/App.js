4d3c2ffc5e05d13281010f8bbde3149d9a07d1d8
srijan67 <srijansinha67@gmail.com>
2023-04-04 16:57:40 +0530

    phase-1 function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import { useState } from "react";
 
 function App() {
+  const [transcript, setTranscript] = useState("");
+  const { finalTranscript, resetTranscript } = useSpeechRecognition();
+
+  const handleListen = () => {
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+      alert("Your browser does not support speech recognition");
+      return;
+    }
+    SpeechRecognition.startListening({ continuous: true });
+  };
+
+  const handleStop = () => {
+    SpeechRecognition.stopListening();
+    console.log("sadfasf", finalTranscript);
+    setTranscript(finalTranscript);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleListen}>Start Listening</button>
+      <button onClick={handleStop}>Stop Listening</button>
+      <button onClick={resetTranscript}>Reset</button>
+      <button
+        onClick={() => {
+          setTranscript();
+        }}
+      >
+        clear
+      </button>
+      <p>This is transcript: {transcript}</p>
     </div>
   );
 }
