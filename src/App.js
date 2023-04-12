b6f902e405ae50f19ece833d68ece933bdd47781
vivek sharma <vivek26jan2000@gmail.com>
2023-04-12 07:43:26 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [transcript, setTranscript] = useState("");
+  const [entities, setEntities] = useState([]);
   const { finalTranscript, resetTranscript } = useSpeechRecognition();
 
   const handleListen = () => {
@@ -18,22 +19,23 @@ function App() {
 
   const handleStop = () => {
     SpeechRecognition.stopListening();
-    console.log("sadfasf", finalTranscript);
+    console.log("Transcript:", finalTranscript);
     setTranscript(finalTranscript);
   };
+
   return (
     <div className="App">
       <button onClick={handleListen}>Start Listening</button>
       <button onClick={handleStop}>Stop Listening</button>
       <button onClick={resetTranscript}>Reset</button>
-      <button
-        onClick={() => {
-          setTranscript();
-        }}
-      >
-        clear
-      </button>
+      <button onClick={() => setTranscript("")}>Clear</button>
       <p>This is transcript: {transcript}</p>
+      <p>Entities:</p>
+      <ul>
+        {entities.map((entity, index) => (
+          <li key={index}>{entity.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
